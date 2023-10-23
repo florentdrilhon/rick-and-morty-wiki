b8abf13554b06b6adbb6219ec53ca768663b26d7
florentdrilhon <florentdrilhonvvs@gmail.com>
2023-10-23 14:50:46 +0800

    [navbar] add menu to access location and episodes pages

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -15,6 +15,35 @@ const Navbar = () => {
             display: none;
           }
         `}</style>
+        <button
+          className="navbar-toggler border-0"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="fas fa-bars open text-dark"></span>
+          <span className="fas fa-times close text-dark"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNavAltMarkup"
+        >
+          {" "}
+          <div className="navbar-nav fs-5">
+            <NavLink to="/" className="nav-link">
+              Characters
+            </NavLink>
+            <NavLink to="/episodes" className="nav-link">
+              Episode
+            </NavLink>
+            <NavLink className="nav-link" to="/location">
+              Location
+            </NavLink>
+          </div>
+        </div>
       </div>
     </nav>
   );
